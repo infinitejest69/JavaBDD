1e7b9c406b353c0dd2d9a676f5e791e74604dbc1
Andy Lumsden <lumsden1@gmail.com>
2021-03-08 00:57:23 +0000

    Bed Time commit lot to do.

---
+++ src/test/java/seleniumTests/TestRunner.java
@@ -0,0 +1,9 @@
+package seleniumTests;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/functionalTests", glue = "seleniumTests")
+public class TestRunner {}
---
+++ src/test/resources/functionalTests/test.feature
@@ -0,0 +1,4 @@
+Feature: Test
+
+  Scenario: Test
+Given user is on Google Home Page
